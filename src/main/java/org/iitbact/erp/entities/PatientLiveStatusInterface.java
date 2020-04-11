527664d96502454c464aa92fe476a86125ed3416
Abhinav Shrestha <abhinav.shrestha@claroenergy.in>
2020-04-11 23:36:37 +0530

    Minor Bug Fixes

--- src/main/java/org/iitbact/erp/entities/PatientLiveStatusInterface.java
+++ src/main/java/org/iitbact/erp/entities/PatientLiveStatusInterface.java
@@ -1,16 +1,18 @@
 package org.iitbact.erp.entities;
 
 public interface PatientLiveStatusInterface {
-	
+
 	public int getFacilityId();
 
 	public String getFacilityName();
 
 	public int getWardId();
 
+	public String getWardName();
+
 	public String getWardType();
 
-	public String getCondition();
+	public String getPatientCondition();
 
 	public String getTestOutcome();
 
