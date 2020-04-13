3544f3a0e0372e66cb41dd101ac8fccf4550f877
Vinod Talapa <vinodtalapa002@gmail.com>
2020-04-14 02:00:08 +0530

    Minor fix

--- src/main/java/org/iitbact/erp/repository/PatientLiveStatusRepository.java
+++ src/main/java/org/iitbact/erp/repository/PatientLiveStatusRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface PatientLiveStatusRepository extends JpaRepository<PatientLiveStatus, Integer> {
 
-	@Query(nativeQuery = true, value = "SELECT pls.patient_id patientID,pls.severity,pls.test_status testStatus,f.name facilityName,w.name wardName,f.facility_id facilityId,IFNULL(w.id,0) wardId FROM patient_live_status pls INNER JOIN facilities f ON f.facility_id=pls.facility_id LEFT JOIN wards w ON w.id=pls.ward_id WHERE pls.patient_id=?1")
+	@Query(nativeQuery = true, value = "SELECT pls.patient_id patientId,pls.severity,pls.test_status testStatus,f.name facilityName,w.name wardName,f.facility_id facilityId,IFNULL(w.id,0) wardId FROM patient_live_status pls INNER JOIN facilities f ON f.facility_id=pls.facility_id LEFT JOIN wards w ON w.id=pls.ward_id WHERE pls.patient_id=?1")
 	PatientLiveStatusInterface findByPatientIdFromMultipleTables(int patientId);
 
 	@Query(nativeQuery = true, value = "SELECT pls.patient_id patientId,p.name patientName,pls.severity,pls.test_status testStatus,f.name facilityName,w.name wardName,f.facility_id facilityId,IFNULL(w.id,0) wardId FROM patient_live_status pls INNER JOIN facilities f ON f.facility_id=pls.facility_id INNER JOIN patients p ON p.patient_id=pls.patient_id  LEFT JOIN wards w ON w.id=pls.ward_id WHERE pls.facility_id=?1")
