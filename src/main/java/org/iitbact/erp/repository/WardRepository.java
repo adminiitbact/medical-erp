b6c1c2132136a307b88ab9b63d65ae1bdfc0dc65
Vinod Talapa <vinodtalapa002@gmail.com>
2020-04-17 18:47:17 +0530

    ward list recommendation added

--- src/main/java/org/iitbact/erp/repository/WardRepository.java
+++ src/main/java/org/iitbact/erp/repository/WardRepository.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.iitbact.erp.entities.Ward;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface WardRepository extends JpaRepository<Ward, Integer> {
+	
+	@Query(nativeQuery = true,value = "SELECT * FROM wards w WHERE w.facility_id=?1 ORDER BY IF(w.severity=?3 AND w.covid_status=?2,1,0) DESC")
 	List<Ward> findByFacilityIdAndCovidStatusAndSeverity(int facilityId, String covidStatus, String severity);
 
 	Ward findByIdAndFacilityId(int wardId, int facilityId);
