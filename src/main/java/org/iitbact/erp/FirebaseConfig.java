f480c1676f2f0ad4f604f52a02e69b80b2da2e76
Vinod Talapa <vinodtalapa002@gmail.com>
2020-04-18 09:26:10 +0530

    Firebase json from system properties

--- src/main/java/org/iitbact/erp/FirebaseConfig.java
+++ src/main/java/org/iitbact/erp/FirebaseConfig.java
@@ -1,11 +1,15 @@
 package org.iitbact.erp;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
 
 import javax.annotation.PostConstruct;
 
+import org.iitbact.erp.constants.SYSTEM_ENVIRONMENT;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ResourceUtils;
 
@@ -16,21 +20,64 @@ import com.google.firebase.FirebaseOptions;
 @Configuration
 public class FirebaseConfig {
 
+	@Value("firebase")
+	private String credentials;
+
+	@Value("firebase.dburl")
+	private String dbUrl;
+
+	@Value("ENV")
+	private String env;
+
 	@PostConstruct
 	public void initFirebase() {
-		try {
+		System.out.println("Firebase credentials "+credentials);
+		System.out.println("firebase.dburl "+dbUrl);
+		System.out.println("env "+ env);
+		if (SYSTEM_ENVIRONMENT.DEV.toString().equalsIgnoreCase(env)) {
+			GoogleCredentials credentials = readCredentialsForDev();
+			FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(credentials).setDatabaseUrl(dbUrl)
+					.build();
+			FirebaseApp.initializeApp(options);
+		} else {
+			GoogleCredentials credentials = readCredentialsFromProperties();
+			FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(credentials).setDatabaseUrl(dbUrl)
+					.build();
+			FirebaseApp.initializeApp(options);
+		}
+	}
 
+	// Google credentials from firebase property from system
+	private GoogleCredentials readCredentialsFromProperties() {
+		try {
+	        //use ByteArrayInputStream to get the bytes of the String and convert them to InputStream.
+	        InputStream inputStream = new ByteArrayInputStream(credentials.getBytes(Charset.forName("UTF-8")));
+			return GoogleCredentials.fromStream(inputStream);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	// Google credentials for Dev using GOOGLE_APPLICATION_CREDENTIALS
+	private GoogleCredentials readCredentialsForDev() {
+		try {
+			return GoogleCredentials.getApplicationDefault();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	// Google credentials from firebase property from system
+	@SuppressWarnings("unused")
+	private GoogleCredentials readCredentialsFromFile() {
+		try {
 			File file = ResourceUtils.getFile("classpath:firebase.json");
 			FileInputStream serviceAccount = new FileInputStream(file);
-
-			FirebaseOptions options = new FirebaseOptions.Builder()
-					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-					.setDatabaseUrl("https://hospital-erp-prod.firebaseio.com").build();
-			FirebaseApp.initializeApp(options);
-			System.out.println(FirebaseApp.DEFAULT_APP_NAME);
-		} catch (IOException e) {
+			return GoogleCredentials.fromStream(serviceAccount);
+		} catch (Exception e) {
 			e.printStackTrace();
+			return null;
 		}
-
 	}
 }
