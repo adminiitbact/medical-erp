c029e50afdad21c50bef3c064e866d161aa0d225
Vinod Talapa <vinodtalapa002@gmail.com>
2020-04-19 16:42:34 +0530

    db changes to prod

--- src/main/resources/static/changes.sql
+++ src/main/resources/static/changes.sql
@@ -1,3 +1,16 @@
+
+
+
+
+
+
+
+
+
+
+
+--------------------------------- Done -------------------------------
+
 ALTER TABLE `facilities`
 	ADD COLUMN `address` TINYTEXT NULL DEFAULT NULL AFTER `is_fever_clinic_available`,
 	ADD COLUMN `ulb_ward_name` VARCHAR(50) NULL DEFAULT NULL AFTER `address`,
