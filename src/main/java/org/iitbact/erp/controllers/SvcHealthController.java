c8008dbe2d1045951f7d98e8642517c888770b06
Vinod Talapa <vinodtalapa002@gmail.com>
2020-04-19 23:17:13 +0530

    Bug Fix Patient Hospital Id

--- src/main/java/org/iitbact/erp/controllers/SvcHealthController.java
+++ src/main/java/org/iitbact/erp/controllers/SvcHealthController.java
@@ -1,8 +1,6 @@
 package org.iitbact.erp.controllers;
 
-import org.iitbact.erp.requests.BaseRequest;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
