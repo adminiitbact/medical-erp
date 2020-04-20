eb20e0ebe88d5b1e220f0d51e01c5e27358527ff
Vinod Talapa <vinodtalapa002@gmail.com>
2020-04-20 11:49:08 +0530

    Done running sql

--- src/main/resources/static/old_changes.sql
+++ src/main/resources/static/old_changes.sql
@@ -1,3 +1,23 @@
+--------------------------------- Done 20/04/2020 ------------------------------
+ALTER TABLE `wards`
+	ADD COLUMN `ventilators_occupied` INT(11) NOT NULL DEFAULT '0' AFTER `ventilators`;
+
+
+ALTER TABLE `wards`
+	DROP COLUMN `icu_beds`;
+ALTER TABLE `wards`
+	ADD COLUMN `covid_ward` BIT NOT NULL DEFAULT b'0' AFTER `extra_fields`;
+
+
+
+ALTER TABLE `wards_history`
+	ADD COLUMN `covid_ward` BIT NOT NULL DEFAULT b'0' AFTER `extra_fields`,
+	ADD COLUMN `ventilators_occupied` INT NOT NULL DEFAULT 0 AFTER `covid_ward`,
+	DROP COLUMN `icu_beds`;
+
+ALTER TABLE `patients`
+	ADD COLUMN `pre_existing_medical_condition` JSON NULL DEFAULT NULL AFTER `goi_covid_id`;
+
 --------------------------------- Done 19/04/2020-------------------------------
 
 ALTER TABLE `facilities`
