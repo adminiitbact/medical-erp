72bc672bcfcf787b6a36ad9fdadcfdc886619d72
Vinod Talapa <vinodtalapa002@gmail.com>
2020-04-20 05:48:30 +0530

    Sql changes

--- src/main/resources/static/changes.sql
+++ src/main/resources/static/changes.sql
@@ -16,11 +16,3 @@ ALTER TABLE `wards_history`
 
 ALTER TABLE `patients`
 	ADD COLUMN `pre_existing_medical_condition` JSON NULL DEFAULT NULL AFTER `goi_covid_id`;
-
---------------------------------- Done -------------------------------
-
-ALTER TABLE `facilities`
-	ADD COLUMN `address` TINYTEXT NULL DEFAULT NULL AFTER `is_fever_clinic_available`,
-	ADD COLUMN `ulb_ward_name` VARCHAR(50) NULL DEFAULT NULL AFTER `address`,
-	ADD COLUMN `ulb_zone_name` VARCHAR(50) NULL DEFAULT NULL AFTER `ulb_ward_name`;
-	
